f84ead7a5b36f39d66e90debea5b16c5d99ed518
Kostas-Mavrogalos <kostasmavrogalos@gmail.com>
2020-12-14 19:04:58 +0200

    I wanna slay a dragon

--- main.c
+++ main.c
@@ -18,14 +18,29 @@ int main () {
   strcpy(record.surname, "cage");
   strcpy(record.address, "sex");
 
-  printf("%d\n", header_info->fileDesc);
-
-  printf("%c\n", header_info->attrType);
-
-  printf("%s\n", header_info->attrName);
-
-  printf("%d\n", header_info->attrLength);
-
   HP_InsertEntry(&header_info, record);
+
+  record.id = 1;
+  strcpy(record.name, "holas");
+  strcpy(record.surname, "cage");
+  strcpy(record.address, "ex");
+
+  HP_InsertEntry(*header_info, record);
+
+  HP_InsertEntry(*header_info, record);
+  HP_InsertEntry(*header_info, record);
+  HP_InsertEntry(*header_info, record);
+  HP_InsertEntry(*header_info, record);
+  HP_InsertEntry(*header_info, record);
+  HP_InsertEntry(*header_info, record);
+  HP_InsertEntry(*header_info, record);
+  HP_InsertEntry(*header_info, record);
+  HP_InsertEntry(*header_info, record);
+  HP_InsertEntry(*header_info, record);
+  HP_InsertEntry(*header_info, record);
+
+  HP_GetAllEntries(*header_info, NULL);
+
+  HP_GetAllEntries(*header_info, 0);
 
 }
