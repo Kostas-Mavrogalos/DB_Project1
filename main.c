f3cf7ed7c193b0c5c7ea81526dab83fcaf46c39f
Kostas-Mavrogalos <kostasmavrogalos@gmail.com>
2020-12-15 01:09:27 +0200

    I WaNNA DIEEEE

--- main.c
+++ main.c
@@ -70,12 +70,211 @@ int main () {
 
   HP_InsertEntry(*header_info, record);
 
+  record.id = 8;
+  strcpy(record.name, "nicholas");
+  strcpy(record.surname, "cage");
+  strcpy(record.address, "sex");
+
+  HP_InsertEntry(*header_info, record);
+
+  record.id = 9;
+  strcpy(record.name, "holas");
+  strcpy(record.surname, "cage");
+  strcpy(record.address, "ex");
+
+  HP_InsertEntry(*header_info, record);
+
+  record.id = 10;
+  strcpy(record.name, "nicholas");
+  strcpy(record.surname, "cage");
+  strcpy(record.address, "sex");
+
+  HP_InsertEntry(*header_info, record);
+
+  record.id = 11;
+  strcpy(record.name, "holas");
+  strcpy(record.surname, "cage");
+  strcpy(record.address, "ex");
+
+  HP_InsertEntry(*header_info, record);
+
+  record.id = 12;
+  strcpy(record.name, "nicholas");
+  strcpy(record.surname, "cage");
+  strcpy(record.address, "sex");
+
+  HP_InsertEntry(*header_info, record);
+
+  record.id = 13;
+  strcpy(record.name, "holas");
+  strcpy(record.surname, "cage");
+  strcpy(record.address, "ex");
+
+  HP_InsertEntry(*header_info, record);
+
+  record.id = 14;
+  strcpy(record.name, "nicholas");
+  strcpy(record.surname, "cage");
+  strcpy(record.address, "sex");
+
+  HP_InsertEntry(*header_info, record);
+
+  record.id = 15;
+  strcpy(record.name, "holas");
+  strcpy(record.surname, "cage");
+  strcpy(record.address, "ex");
+
+  HP_InsertEntry(*header_info, record);
+
 
   int value = 0;
   HP_GetAllEntries(*header_info, (void*)&value);
 
-  value = 1;
-  HP_GetAllEntries(*header_info, (void*)&value);
+  record.id = 1;
+  strcpy(record.name, "holas");
+  strcpy(record.surname, "cage");
+  strcpy(record.address, "ex");
+printf("ASDASDSADD\n");
+  void * blank;
 
-  HP_GetAllEntries(*header_info, NULL);
+BF_ReadBlock(header_info->fileDesc, 1, &blank);
+memcpy(&record, blank, sizeof(Record));
+
+printf("%d\n", record.id);
+printf("%s\n", record.name);
+printf("%s\n", record.surname);
+printf("%sBEFORE\n\n", record.address);
+
+blank += sizeof(Record);
+memcpy(&record, blank, sizeof(Record));
+
+printf("%d\n", record.id);
+printf("%s\n", record.name);
+printf("%s\n", record.surname);
+printf("%s\n", record.address);
+
+blank += sizeof(Record);
+memcpy(&record, blank, sizeof(Record));
+
+printf("%d\n", record.id);
+printf("%s\n", record.name);
+printf("%s\n", record.surname);
+printf("%s\n", record.address);
+
+blank += sizeof(Record);
+memcpy(&record, blank, sizeof(Record));
+
+printf("%d\n", record.id);
+printf("%s\n", record.name);
+printf("%s\n", record.surname);
+printf("%s\n", record.address);
+
+blank += sizeof(Record);
+memcpy(&record, blank, sizeof(Record));
+
+printf("%d\n", record.id);
+printf("%s\n", record.name);
+printf("%s\n", record.surname);
+printf("%s\n", record.address);
+
+
+BF_ReadBlock(header_info->fileDesc, 2, &blank);
+blank += BLOCK_SIZE - sizeof(int);
+int kapou;
+memcpy(&kapou, blank, sizeof(int));
+printf("%d KAPOYYYYYYYYYYYYYY\n", kapou);
+blank += sizeof(int) - BLOCK_SIZE;
+
+blank += sizeof(Record);
+memcpy(&record, blank, sizeof(Record));
+
+printf("%d\n", record.id);
+printf("%s\n", record.name);
+printf("%s\n", record.surname);
+printf("%s\n", record.address);
+
+blank += sizeof(Record);
+memcpy(&record, blank, sizeof(Record));
+
+printf("%d\n", record.id);
+printf("%s\n", record.name);
+printf("%s\n", record.surname);
+printf("%s\n", record.address);
+
+blank += sizeof(Record);
+memcpy(&record, blank, sizeof(Record));
+
+printf("%d\n", record.id);
+printf("%s\n", record.name);
+printf("%s\n", record.surname);
+printf("%s\n", record.address);
+
+blank += sizeof(Record);
+memcpy(&record, blank, sizeof(Record));
+
+printf("%d\n", record.id);
+printf("%s\n", record.name);
+printf("%s\n", record.surname);
+printf("%s\n", record.address);
+
+blank += sizeof(Record);
+memcpy(&record, blank, sizeof(Record));
+
+printf("%d\n", record.id);
+printf("%s\n", record.name);
+printf("%s\n", record.surname);
+printf("%s\n", record.address);
+
+BF_ReadBlock(header_info->fileDesc, 3, &blank);
+blank += BLOCK_SIZE - sizeof(int);
+memcpy(&kapou, blank, sizeof(int));
+printf("%d KAPOYYYYYYYYYYYYYY\n", kapou);
+blank += sizeof(int) - BLOCK_SIZE;
+
+blank += sizeof(Record);
+memcpy(&record, blank, sizeof(Record));
+
+printf("%d\n", record.id);
+printf("%s\n", record.name);
+printf("%s\n", record.surname);
+printf("%s\n", record.address);
+
+blank += sizeof(Record);
+memcpy(&record, blank, sizeof(Record));
+
+printf("%d\n", record.id);
+printf("%s\n", record.name);
+printf("%s\n", record.surname);
+printf("%s\n", record.address);
+
+blank += sizeof(Record);
+memcpy(&record, blank, sizeof(Record));
+
+printf("%d\n", record.id);
+printf("%s\n", record.name);
+printf("%s\n", record.surname);
+printf("%s\n", record.address);
+
+blank += sizeof(Record);
+memcpy(&record, blank, sizeof(Record));
+
+printf("%d\n", record.id);
+printf("%s\n", record.name);
+printf("%s\n", record.surname);
+printf("%s\n", record.address);
+
+blank += sizeof(Record);
+memcpy(&record, blank, sizeof(Record));
+
+printf("%d\n", record.id);
+printf("%s\n", record.name);
+printf("%s\n", record.surname);
+printf("%s\n", record.address);
+
+
+  value = 1;
+//  HP_GetAllEntries(*header_info, (void*)&value);
+
+printf("asdaasdadasdasd\n");
+  //HP_GetAllEntries(*header_info, NULL);
 }
