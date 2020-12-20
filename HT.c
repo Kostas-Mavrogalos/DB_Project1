fce6184ff9323b471e631a8610a1b9fc8a9bf121
kontino <61058959+kontino@users.noreply.github.com>
2020-12-21 00:06:09 +0200

    Update HT.c

--- HT.c
+++ HT.c
@@ -23,62 +23,56 @@ int HT_CreateIndex(char* filename, char attrType, char* attrName, int attrLength
 		BF_PrintError("Couldn't open file");
 		return -1;
 	}
+	
+	// Use block #0 as header of the hash file.
+ 	if (BF_AllocateBlock(fileDesc) < 0 ) {
+		BF_PrintError("Couldn't allocate block");
+		return -1;
+	}
 
-  // HASH!!!
-  // KAPWS EPISTREFETAI TO BUCKET 1
-  for (int i=0; i< HASH_LIMIT; i++) {
-    if (BF_AllocateBlock(i) < 0 ) {
-  		BF_PrintError("Couldn't allocate block");
-  		return -1;
+ 	if (BF_ReadBlock(fileDesc, 0, &block) < 0 ) {
+		BF_PrintError("Couldn't read block");
+		return -1;
+	}
+	
+	// Store all necessary data for HT_info which is initialized later
+	memcpy(block, &fileDesc, sizeof(int));
+	block += sizeof(int);
+
+	memcpy(block, &attrType, sizeof(char));
+	block += sizeof(char);
+
+	strcpy(block, attrName);
+	block += strlen(attrName) + 1;
+
+	memcpy(block, &attrLength, sizeof(int));
+	block += sizeof(int);
+	
+	memcpy(block, &numBuckets, sizeof(long int));
+
+	if (BF_WriteBlock(fileDesc, 0) < 0){
+		BF_PrintError("Couldnt' write block");
+		return -1;
+	}
+
+ 	// From block #1 up to block #numBuckets, create the first block of each bucket
+  	for (int i=1; i <= numBuckets; i++) {
+    		if (BF_AllocateBlock(i) < 0 ) {
+  			BF_PrintError("Couldn't allocate block");
+  			return -1;
+  		}
+
+   		if (BF_ReadBlock(i, 0, &block) < 0 ) {
+  			BF_PrintError("Couldn't read block");
+  			return -1;
+  		}
+
+  		if (BF_WriteBlock(i, 0) < 0){
+  			BF_PrintError("Couldnt' write block");
+  			return -1;
+  		}
+
   	}
-
-   	if (BF_ReadBlock(i, 0, &block) < 0 ) {
-  		BF_PrintError("Couldn't read block");
-  		return -1;
-  	}
-
-  	memcpy(block, &i, sizeof(int));
-  	block += sizeof(int);
-
-  	memcpy(block, &attrType, sizeof(char));
-  	block += sizeof(char);
-
-  	strcpy(block, attrName);
-  	block += strlen(attrName) + 1;
-
-  	memcpy(block, &attrLength, sizeof(int));
-
-  	if (BF_WriteBlock(i, 0) < 0){
-  		BF_PrintError("Couldnt' write block");
-  		return -1;
-  	}
-   	// if (BF_AllocateBlock(fileDesc) < 0 ) {
-  	// 	BF_PrintError("Couldn't allocate block");
-  	// 	return -1;
-  	// }
-    //
-   	// if (BF_ReadBlock(fileDesc, 0, &block) < 0 ) {
-  	// 	BF_PrintError("Couldn't read block");
-  	// 	return -1;
-  	// }
-    //
-  	// memcpy(block, &fileDesc, sizeof(int));
-  	// block += sizeof(int);
-    //
-  	// memcpy(block, &attrType, sizeof(char));
-  	// block += sizeof(char);
-    //
-  	// strcpy(block, attrName);
-  	// block += strlen(attrName) + 1;
-    //
-  	// memcpy(block, &attrLength, sizeof(int));
-    //
-  	// if (BF_WriteBlock(fileDesc, 0) < 0){
-  	// 	BF_PrintError("Couldnt' write block");
-  	// 	return -1;
-  	// }
-  }
-
 
 	if (BF_CloseFile(fileDesc)< 0 ) {
 		BF_PrintError("Couldn't close file");
