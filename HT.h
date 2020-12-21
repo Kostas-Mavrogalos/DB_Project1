f11fee4f551a48b42f1a08e9e301e941dec57053
Kostas-Mavrogalos <kostasmavrogalos@gmail.com>
2020-12-21 22:35:48 +0200

    It's the final countdown

---
+++ HT.h
@@ -0,0 +1,26 @@
+#include "Record.h"
+#pragma once
+
+typedef struct {
+  int fileDesc;
+  char attrType;
+  char attrName[20];
+  int attrLength;
+  long int numBuckets;
+}HT_info;
+
+int hashFunction(long int numBuckets, void* id);
+
+int HT_CreateIndex(char* filename, char attrType, char* attrName, int attrLength, long int numBuckets);
+
+HT_info* HT_OpenIndex(char* filename);
+
+int HT_CloseIndex(HT_info *header_info);
+
+int HT_InsertEntry(HT_info header_info, Record record);
+
+int HT_DeleteEntry(HT_info header_info, void* value);
+
+int HT_GetAllEntries(HT_info header_info, void* value);
+
+int HashStatistic(char* filename);
--- main.c
+++ main.c
@@ -4,15 +4,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "HT.h"
 
 int main () {
   char* filename = "sad.txt";
 	void *block;
-  BF_Init();
-  HP_CreateFile(filename, 'i', "DDA", 4);
 
-  HP_info* header_info = HP_OpenFile(filename);
+  HT_CreateIndex(filename, 'i', "DDA", 4, 500);
 
+  HT_info* header_info = HT_OpenIndex(filename);
+
+  int index;
+  BF_ReadBlock(header_info->fileDesc, 1, &block);
+  void* next;
+  int block_number;
+  next = block;
+  next += BLOCK_SIZE - sizeof(int);
+  memcpy(&block_number, next, sizeof(int));
+  printf("%d BLOCK #\n", block_number);
+  BF_ReadBlock(header_info->fileDesc, block_number, &block);
+  for (long int i=0; i<10; i++) {
+    memcpy(&index, (int*)block, sizeof(int));
+    printf("%d INDEX\n", index);
+    block+= sizeof(int);
+  }
   Record record;
 
   record.id = 0;
@@ -20,56 +35,56 @@ int main () {
   strcpy(record.surname, "cage");
   strcpy(record.address, "sex");
 
-  HP_InsertEntry(*header_info, record);
+  HT_InsertEntry(*header_info, record);
 
   record.id = 1;
   strcpy(record.name, "holas");
   strcpy(record.surname, "cage");
   strcpy(record.address, "ex");
 
-  HP_InsertEntry(*header_info, record);
+  HT_InsertEntry(*header_info, record);
 
   record.id = 2;
   strcpy(record.name, "nicholas");
   strcpy(record.surname, "cage");
   strcpy(record.address, "sex");
 
-  HP_InsertEntry(*header_info, record);
+  HT_InsertEntry(*header_info, record);
 
   record.id = 3;
   strcpy(record.name, "holas");
   strcpy(record.surname, "cage");
   strcpy(record.address, "ex");
 
-  HP_InsertEntry(*header_info, record);
+  HT_InsertEntry(*header_info, record);
 
   record.id = 4;
   strcpy(record.name, "nicholas");
   strcpy(record.surname, "cage");
   strcpy(record.address, "sex");
 
-  HP_InsertEntry(*header_info, record);
+  HT_InsertEntry(*header_info, record);
 
   record.id = 5;
   strcpy(record.name, "holas");
   strcpy(record.surname, "cage");
   strcpy(record.address, "ex");
 
-  HP_InsertEntry(*header_info, record);
+  HT_InsertEntry(*header_info, record);
 
   record.id = 6;
   strcpy(record.name, "nicholas");
   strcpy(record.surname, "cage");
   strcpy(record.address, "sex");
 
-  HP_InsertEntry(*header_info, record);
+  HT_InsertEntry(*header_info, record);
 
   record.id = 7;
   strcpy(record.name, "holas");
   strcpy(record.surname, "cage");
   strcpy(record.address, "ex");
 
-  HP_InsertEntry(*header_info, record);
+  HT_InsertEntry(*header_info, record);
 
   int val = 2;
   Record rec;
@@ -77,7 +92,7 @@ int main () {
   BF_ReadBlock(header_info->fileDesc, 1, &block);
   memcpy(&rec, block, sizeof(Record));
 
-  HP_DeleteEntry(*header_info, &val);
+  HT_DeleteEntry(*header_info, &val);
   BF_ReadBlock(header_info->fileDesc, 1, &block);
   memcpy(&rec, block, sizeof(Record));
 
@@ -93,6 +108,7 @@ int main () {
   printf("Id: %d\n", rec.id);
   printf("Name: %s\n", rec.name);
   val = 1;
- int* dick = NULL;
-  HP_GetAllEntries(*header_info, dick);
+  int* dick = NULL;
+  printf("ALL MOTHEFUCKAAAA\n");
+  HT_GetAllEntries(*header_info, dick);
 }
