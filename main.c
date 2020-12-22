713bd651589ec348fc849d09066730352555ddcf
kontino <61058959+kontino@users.noreply.github.com>
2020-12-22 02:38:49 +0200

    Update main.c

--- main.c
+++ main.c
@@ -38,7 +38,7 @@ int main () {
   printf("----------------------------------\n");
 
 BF_ReadBlock(header_info->fileDesc, 1, block);
-block += 3*sizeof(int);
+block += sizeof(int);
 memcpy(&dick, block, sizeof(int));
 printf("dick = %d\n\n\n", dick);
 	
